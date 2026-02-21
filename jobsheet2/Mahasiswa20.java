9a01ce62d2c9ce73245f049170c857c53c67a00f
Naufalentino <naufalvalentino422@gmail.com>
2026-02-21 16:17:26 +0700

    --

--- jobsheet2/Mahasiswa20.java
+++ jobsheet2/Mahasiswa20.java
@@ -29,23 +29,25 @@ class Mahasiswa20 {
     }
 
     void updateIpk (double ipkBaru){
-        ipk = ipkBaru;
+        if (ipkBaru>=0.0&&ipkBaru<=4.0) {
+            ipk = ipkBaru;
+        } else {
+            System.out.println("IPK tidak valid. Harus diantara 0.0 dan 4.0");
+        }
     }
 
     String nilaiKinerja (){
-        if (ipk>=0&&ipk<=4) {
-            if (ipk >= 3.5) {
-                return "Kinerja sangat baik";
-            } else if (ipk>=3.0) {
-                return "Kinerja baik";
-            } else if (ipk >= 2.0) {
-                return "Kinerja cukup";
-            } else {
-                return "Kinerja kurang";
-            }
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        } else if (ipk>=3.0) {
+            return "Kinerja baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja cukup";
         } else {
-            return "IPK tidak valid. Harus diantara 0.0 dan 4.0";
+            return "Kinerja kurang";
         }
+        
     }
 }
+
 
