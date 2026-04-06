3d5bc616ad6ef5311fb67cfa1955074f1042fe39
Naufalentino <naufalvalentino422@gmail.com>
2026-04-07 06:01:02 +0700

    Mengubah insertion sort menjadi descending

--- nguawor/MahasiswaBerprestasi20.java
+++ nguawor/MahasiswaBerprestasi20.java
@@ -48,7 +48,7 @@ public class MahasiswaBerprestasi20{
         for (int i = 1; i < listMhs.length; i++) {
             Mahasiswa_20 temp = listMhs[i];
             int j=i;
-            while (j>0 && listMhs[j-1].ipk20>temp.ipk20) {
+            while (j>0 && listMhs[j-1].ipk20<temp.ipk20) {
                 listMhs[j]=listMhs[j-1];
                 j--;
             }
