5a762a0812b31c0c04ad2de9a1d050a53daf70df
Naufalentino <naufalvalentino422@gmail.com>
2026-04-06 19:36:48 +0700

    modifikasi supaya input dari terminal

--- nguawor/MahasiswaDemo_20.java
+++ nguawor/MahasiswaDemo_20.java
@@ -1,17 +1,24 @@
+import java.util.Scanner;
+
 public class MahasiswaDemo_20 {
     public static void main(String[] args) {
+        Scanner tino = new Scanner(System.in);
         MahasiswaBerprestasi20 list = new MahasiswaBerprestasi20();
-        Mahasiswa_20 m1 = new Mahasiswa_20("123", "Zidan", "2A", 3.2);
-        Mahasiswa_20 m2 = new Mahasiswa_20("124", "Ayu", "2A", 3.5);
-        Mahasiswa_20 m3 = new Mahasiswa_20("125", "Sofi", "2A", 3.1);
-        Mahasiswa_20 m4 = new Mahasiswa_20("126", "Sita", "2A", 3.9);
-        Mahasiswa_20 m5 = new Mahasiswa_20("127", "Miki", "2A", 3.7);
-
-        list.tambah(m1);
-        list.tambah(m2);
-        list.tambah(m3);
-        list.tambah(m4);
-        list.tambah(m5);
+        for(int i=0; i < 5; i++) {
+            System.out.println("Masukkan Data Mahasiswa ke-" + (i+1));
+            System.out.print("NIM     : ");
+            String nim =tino.nextLine();
+            System.out.print("Nama    : ");
+            String nama = tino.nextLine();
+            System.out.print("Kelas   : ");
+            String kelas = tino.nextLine();
+            System.out.print("IPK     : ");
+            String dummy = tino.nextLine();
+            double ipk = Double.parseDouble(dummy);
+            System.out.println("-----------------------------------");
+            Mahasiswa_20 m = new Mahasiswa_20(nim,nama,kelas,ipk);
+            list.tambah(m);
+        }
 
         System.out.println("Data Mahasiswa belum di sorting: ");
         list.tampil();
@@ -19,5 +26,7 @@ public class MahasiswaDemo_20 {
         System.out.println("Data Mahasiswa setelah sorting berdasarkan IPK (DESC)");
         list.bubbleSort();
         list.tampil();
+
+        tino.close();
     }
 }
