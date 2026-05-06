ad8bb922762116c1169d28000ba8d9591cd82780
Naufalentino <naufalvalentino422@gmail.com>
2026-05-06 08:00:43 +0700

    modifikasi pertanyaan percobaan 1 nomor 2

--- jobsheet11/SLLMain20.java
+++ jobsheet11/SLLMain20.java
@@ -1,18 +1,25 @@
+import java.util.Scanner;
+
 public class SLLMain20 {
     public static void main(String[] args) {
+        Scanner tino = new Scanner(System.in);
         SingleLinkedList20 sll = new SingleLinkedList20();
-        Mahasiswa020 mhs1 = new Mahasiswa020("24212200","Alvaro","1A",4.0);
-        Mahasiswa020 mhs2 = new Mahasiswa020("23020101","Bimon","2B",3.8);
-        Mahasiswa020 mhs3 = new Mahasiswa020("22212202","Cintia","3C",3.5);
-        Mahasiswa020 mhs4 = new Mahasiswa020("21212203","Dirga","4D",3.6);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Masukkan Data Mahasiswa ke-" + (i+1));
+            System.out.print("Nama    : ");
+            String nama = tino.nextLine();
+            System.out.print("NIM     : ");
+            String nim =tino.nextLine();
+            System.out.print("Kelas   : ");
+            String kelas = tino.nextLine();
+            System.out.print("IPK     : ");
+            String dummy = tino.nextLine();
+            double ipk = Double.parseDouble(dummy);
+            System.out.println("-----------------------------------");
+            Mahasiswa020 m = new Mahasiswa020(nim,nama,kelas,ipk);
+            sll.addFirst(m);
+        }
 
-        sll.print();
-        sll.addFirst(mhs4);
-        sll.print();
-        sll.addLast(mhs1);
-        sll.print();
-        sll.insertAfter("Dirga", mhs3);
-        sll.insertAt(2, mhs2);
-        sll.print();
+        tino.close();
     }
 }
