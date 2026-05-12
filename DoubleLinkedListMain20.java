657fe753ca7927f57737656b0ac87b8ece0427bb
Naufalentino <naufalvalentino422@gmail.com>
2026-05-12 12:00:19 +0700

    P12 tugas_praktikum berupa penambahan

--- DoubleLinkedListMain20.java
+++ DoubleLinkedListMain20.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class DoubleLinkedListMain20 {
     static Maahaasiiswaa20 inputMahasiswa(Scanner tino){
+        tino.nextLine();
         System.out.print("Masukkan NIM   : ");
         String nim = tino.nextLine();
         System.out.print("Masukkan Nama  : ");
@@ -20,13 +21,20 @@ public class DoubleLinkedListMain20 {
         int pilihan = 0;
 
         do {
-            System.out.println("\n===== MENU DOUBLE LINKED LISY =====");
+            System.out.println("\n===== MENU DOUBLE LINKED LIST =====");
             System.out.println("1. Tambah data di awal");
             System.out.println("2. Tambah data di akhir");
             System.out.println("3. Sisipkan data di tengah (setelah NIM)");
-            System.out.println("4. Hapus data di awal");
-            System.out.println("5. Hapus data di akhir");
-            System.out.println("6. Tampilkan data");
+            System.out.println("4. Sisipkan data di tengah (berdasarkan index)");
+            System.out.println("5. Hapus data di awal");
+            System.out.println("6. Hapus data di akhir");
+            System.out.println("7. Hapus data di tengah (setelah NIM)");
+            System.out.println("8. Hapus data di tengah (pada indeks nya)");
+            System.out.println("9. Tampilkan data");
+            System.out.println("10. Tampilkan data pertama");
+            System.out.println("11. Tampilkan data terakhir");
+            System.out.println("12. Tampilkan data (berdasarkan index)");
+            System.out.println("13. Tampilkan jumlah data");
             System.out.println("0. Keluar");
             System.out.print("Pilih menu : ");
             pilihan = tino.nextInt();
@@ -67,15 +75,46 @@ public class DoubleLinkedListMain20 {
                     list.insertAfter(keyNim, dataBaru);
                     break;
                 case 4:
-                    list.removeFirst();
+                    System.out.print("Masukkan Index yang ingin dicari: ");
+                    int keyIdx = tino.nextInt();
+                    System.out.println("Masukkan data baru:");
+                    Maahaasiiswaa20 dataIdx = inputMahasiswa(tino);
+                    list.add(keyIdx, dataIdx);
                     break;
                 case 5:
-                    list.removeLast();
+                    list.removeFirst();
                     break;
                 case 6:
+                    list.removeLast();
+                    break;
+                case 7:
+                    System.out.print("Masukkan NIM yang dicari : ");
+                    String key = tino.nextLine();
+                    list.removeAfter(key);
+                    break;
+                case 8:
+                    System.out.print("Masukkan index data yang ingin dihapus : ");
+                    int idx = tino.nextInt();
+                    list.remove(idx);
+                    break;
+                case 9:
                     list.print();
                     System.out.println();
                     list.printReverse();
+                    break;
+                case 10:
+                    list.getFirst();
+                    break;
+                case 11:
+                    list.getLast();
+                    break;
+                case 12:
+                    System.out.print("Masukkan index data yang ingin ditampilkan : ");
+                    int index = tino.nextInt();
+                    list.getIndex(index);
+                    break;
+                case 13:
+                    list.jumlahData();
                     break;
                 case 0:
                     System.out.println("Program selesai.");
--- DoublelinkedList20.java
+++ DoublelinkedList20.java
@@ -18,6 +18,7 @@ public class DoublelinkedList20 {
             while (tmp != null) {
                 tmp.data.tampilInformasi();
                 tmp = tmp.next;
+                System.out.println("-----");
             }
             System.out.println("");
         } else {
@@ -32,10 +33,43 @@ public class DoublelinkedList20 {
             while (tmp != null) {
                 tmp.data.tampilInformasi();
                 tmp = tmp.prev;
+                System.out.println("-----");
             }
             System.out.println("");
         } else {
             System.out.println("Linked list  masih kosong");
+        }
+    }
+
+    public void add(int index,Maahaasiiswaa20 data){
+        if (index < 0) {
+            System.out.println("Indeks salah");
+        } else if (index == 0) {
+            addFirst(data);
+        } else {
+            Node20 temp = head;
+            Node20 newNode = new Node20(data);
+            int i = 0;
+            do {
+                if (temp.next == null && i==index-1) {
+                    addLast(data);
+                    System.out.println("Data berhasil disisipkan pada index setelah "+index);
+                    break;
+                } else if (temp.next != null && i==index-1) {
+                    newNode.prev = temp;
+                    newNode.next = temp.next;
+                    temp.next.prev = newNode;
+                    temp.next = newNode;
+                    System.out.println("Data berhasil disisipkan pada index setelah "+index);
+                    break;
+                }
+                i++;
+                temp = temp.next;
+                if (temp == null) {
+                    System.out.println("Data dengan index "+index+" tidak ditemukan. ");
+                    return;
+                }
+            }while (i < index -1 );
         }
     }
 
@@ -119,5 +153,84 @@ public class DoublelinkedList20 {
             System.out.println("Data berhasil dihapus.");
             tmp.data.tampilInformasi();
         }
+    }
+
+    public void removeAfter(String keyNim){
+        Node20 temp = head;
+        Node20 tmp = temp.next;
+        do {
+            if (temp.data.nim.equals(keyNim) && temp.next != null) {
+                if (temp.next.next == null) {
+                    removeLast();
+                } else if (temp.next != null) {
+                    temp.next = temp.next.next;
+                    temp.next.prev = temp;
+                    System.out.println("Data berhasil dihapus.");
+                    tmp.data.tampilInformasi();
+                }
+                break;
+            } else if (temp.data.nim.equals(keyNim) && temp.next == null) {
+                 System.out.println("Data masih kosong setelah "+keyNim);
+                 break;
+            }
+            temp = temp.next;
+            tmp = tmp.next;
+            if (temp == null) {
+                System.out.println("Data dengan NIM "+keyNim+" tidak ditemukan. ");
+                return;
+            }
+        }while (temp != null);
+    }
+
+    public void remove(int index){
+        if (index < 0) {
+            System.out.println("Indeks salah");
+        } else if (index == 1 && index == 0) {
+            removeFirst();
+        } else {
+            Node20 temp = head;
+            Node20 tmp = temp;
+            for (int i = 0; i < index - 1; i++) {
+                temp = temp.next;
+                tmp = temp;
+            }
+            if (temp.next != null) {
+                temp.next.prev = temp.prev;
+                temp.prev.next = temp.next;
+                System.out.println("Data berhasil dihapus.");
+                tmp.data.tampilInformasi();
+            } else {
+                removeLast();
+            }
+        }
+    }
+
+    public void getFirst(){
+        System.out.println("Data pada node pertama : ");
+        head.data.tampilInformasi();
+    }
+
+    public void getLast(){
+        System.out.println("Data pada node akhir : ");
+        tail.data.tampilInformasi();
+    }
+
+    public void getIndex(int index){
+        Node20 temp = head;
+            for (int i = 0; i < index - 1; i++) {
+                temp = temp.next;
+            }
+        System.out.println("Data pada node dengan indeks "+index+" : ");
+        temp.data.tampilInformasi();
+    }
+
+    public void jumlahData(){
+        Node20 tmp = head;
+        int count = 0;
+        while (tmp != null) {
+            count+=1;
+            tmp = tmp.next;
+        }
+        System.out.println("Jumlah data yang ada pada double linked list adalah "+ count);
     }
 }
