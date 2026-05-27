11ca049e206a11b4740ecbd20c79f8eda29f854a
Naufalentino <naufalvalentino422@gmail.com>
2026-05-27 23:19:03 +0700

    P14 percobaan_1

---
+++ jobsheet14/BinaryTree20.java
@@ -0,0 +1,167 @@
+public class BinaryTree20 {
+    Node020 root;
+
+    public BinaryTree20() {
+        root = null;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public void add(Mahasiswa200 mahasiswa) {
+        Node020 newNode = new Node020(mahasiswa);
+        if (isEmpty()) {
+            root = newNode;
+        } else {
+            Node020 current = root;
+            Node020 parent = null;
+            while (true) {
+                parent = current;
+                if (mahasiswa.ipk < current.mahasiswa.ipk) {
+                    current = current.left;
+                    if (current==null) {
+                        parent.left = newNode;
+                        return;
+                    }
+                } else {
+                    current = current.right;
+                    if (current==null) {
+                        parent.right = newNode;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    boolean find(double ipk){
+        boolean result = false;
+        Node020 current = root;
+        while (current != null) {
+            if (ipk == current.mahasiswa.ipk) {
+                result = true;
+                break;
+            } else if (ipk > current.mahasiswa.ipk) {
+                current = current.right;
+            } else {
+                current = current.left;
+            }
+        }
+        return result;
+    }
+
+    void traversePreOrder(Node020 node) {
+        if (node != null) {
+            node.mahasiswa.tampilInformasi();
+            traversePreOrder(node.left);
+            traversePreOrder(node.right);
+        }
+    }
+
+    void traverseInOrder(Node020 node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            node.mahasiswa.tampilInformasi();
+            traverseInOrder(node.right);
+        }
+    }
+
+    void traversePostOrder(Node020 node) {
+        if (node != null) {
+            traversePostOrder(node.left);
+            traversePostOrder(node.right);
+            node.mahasiswa.tampilInformasi();
+        }
+    }
+
+    Node020 getSuccessor(Node020 del) {
+        Node020 successor = del.right;
+        Node020 successorParent = del;
+        while (successor.left != null) {
+            successorParent = successor;
+            successor = successor.left;
+        }
+        if (successor != del.right) {
+            successorParent.left = successor.right;
+            successor.right = del.right;
+        }
+        return successor;
+    }
+
+    void delete(Node020 node, double ipk) {
+        if (isEmpty()) {
+            System.out.println("Binary tree kosong");
+            return;
+        }
+        
+        Node020 parent = null;
+        Node020 current = root;
+        boolean isLeftChild = false;
+        while (current != null) {
+            if (ipk == current.mahasiswa.ipk) {
+                break;
+            } else if (ipk > current.mahasiswa.ipk) {
+                parent = current;
+                current = current.right;
+                isLeftChild = false;
+            } else {
+                parent = current;
+                current = current.left;
+                isLeftChild = true;
+            }
+        }
+        if (current == null) {
+            System.out.println("Data tidak ditemukan");
+            return;
+        } else {
+            if (current.left == null && current.right == null) {
+                if (current==root) {
+                    root = null;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = null;
+                    } else {
+                        parent.right = null;
+                    }
+                }
+            } else if (current.left == null) {
+                if (current == root) {
+                    root = current.right;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = current.right;
+                    } else {
+                        parent.right = current.right;
+                    }
+                }
+            } else if (current.right == null) {
+                if (current == root) {
+                    root = current.left;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = current.left;
+                    } else {
+                        parent.right = current.left;
+                    }
+                }
+            } else {
+                Node020 successor = getSuccessor(current);
+                System.out.println("Jika 2 anak, current = ");
+                successor.mahasiswa.tampilInformasi();
+                if (current == root) {
+                    root = successor;
+                } else {
+                    if (isLeftChild) {
+                        parent.left = successor;
+                    } else {
+                        parent.right = successor;
+                    }
+                }
+                successor.left = current.left;
+            }
+        }
+        
+    }
+    
+}
---
+++ jobsheet14/Node020.java
@@ -0,0 +1,11 @@
+public class Node020 {
+    Mahasiswa200 mahasiswa;
+    Node020 left, right;
+
+    public Node020(){}
+
+    public Node020(Mahasiswa200 mahasiswa) {
+        this.mahasiswa = mahasiswa;
+        left = right = null;
+    }
+}
