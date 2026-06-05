85c0210ff80e3dc8daa53d57f18af4d87c59f478
Naufalentino <naufalvalentino422@gmail.com>
2026-06-05 22:42:57 +0700

    P15 Praktikum_1

---
+++ jobsheet15/DemoArrayList20.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+public class DemoArrayList20{
+    public static void main(String[] args) {
+        ArrayList<Customer20> customers = new ArrayList<>(2);
+        
+        Customer20 customer1 = new Customer20(1, "Zakia");
+        Customer20 customer2 = new Customer20(2, "Budi");
+        
+        customers.add(customer1);
+        customers.add(customer2);
+    }
+}
